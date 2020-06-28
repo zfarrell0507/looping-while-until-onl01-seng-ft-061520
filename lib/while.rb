7bec8fcbfe43424a73762d2c4167eb1f210c7e59
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-28 17:17:05 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -2,7 +2,7 @@ def using_while
   levitation_force = 6
 	looping_string = "Wingardium Leviosa"
   
-  while levitation_force <= 10
+  while levitation_force < 10
     puts looping_string
     levitation_force += 1
   end
